ec5e2a0be551b534aa15675534e5232ce0790026
HarryChanti <harishcr66@gmail.com>
2023-06-02 11:41:06 +0000

    jenkinfile

---
+++ jenkinfile.sh
@@ -0,0 +1,30 @@
+pipeline {
+  agent any
+  environment {
+     DOCKERHUB_CREDENTIALS = credentials ('dockerhub')
+  }
+  stages {
+      stage('build'){
+      steps {
+          sh 'docker build -t dockerhchanti/jenkins-docker-hub:2 . '
+       }
+       }
+   stage('login') {
+   steps {
+    sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
+   }
+   }
+   stage('push') {
+     steps {
+         sh 'docker push dockerhchanti/jenkins-docker-hub:2
+   }
+   }
+
+}
+}
+post {
+        always {
+            sh 'docker logout'
+        }
+    }
+}  
